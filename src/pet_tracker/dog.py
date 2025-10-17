26a4713f91f9efffd146d3be06f3088bc7b8c8b9
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2025-10-17 16:35:19 -0700

    adding pet and dog classes
    The current focus is on class attributes v instance attributes

---
+++ src/pet_tracker/dog.py
@@ -0,0 +1,38 @@
+"""
+dog.py
+by Chris Winikka
+A Dog object based on the Pet
+"""
+from pet import Pet
+
+class Dog(Pet):
+    count = 0
+    def __init__(self, name, breed, age):
+        super().__init__()
+
+        # add to the count of pets
+        Pet.count += 1
+        Dog.count += 1
+
+        # Set instance attributes
+        self.name = name
+        self.breed = breed
+        self.age = age
+    
+    def __str__(self):
+        representation = f"Name: {self.name}\n"
+        representation += f"Breed: {self.breed}\n"
+        representation += f"Age: {self.age}\n"
+        representation += f"1 of {Dog.count} dogs.\n"
+        representation += f"1 of {Pet.count} pets."
+        return representation
+
+
+if __name__ == "__main__":
+    # Add some dogs
+    fluffy = Dog("Fluffy", "Terrier", 4)
+    sandy = Dog("Sandy", "Cocker Spaniel", 3)
+
+    # Print dog information
+    print(fluffy)
+    print(sandy)
---
+++ src/pet_tracker/pet.py
@@ -0,0 +1,13 @@
+"""
+pet.py
+By Chris Winikka
+A base class for all pets.
+"""
+
+
+class Pet():
+    count = 0
+
+
+if __name__ == "__main__":
+    print(Pet.count)
